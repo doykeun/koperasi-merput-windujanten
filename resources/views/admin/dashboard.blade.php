fffe9262298a996108edafb1c78460059c310d29
Anday <andaykomara3@gmail.com>
2026-06-27 14:06:54 +0700

    update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.admin')
 
+@section('title', 'Dashboard')
+
 @section('content')
 <div class="container-fluid">
     <h1 class="h3 mb-4 text-gray-800">Dashboard Admin</h1>
--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.admin')
 
+@section('title', 'Profil Saya')
+
 @section('content')
 <div class="container-fluid">
     <h1 class="h3 mb-4 text-gray-800">Profil Saya</h1>
